10a6048c7c30817b962c4aed5e520d9315162bd6
Corey Duffy <c.duffy@kainos.com>
2018-06-29 14:08:45 +0100

    Storing location and expiry date in cookie

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 	$('#locations').on('change', function() {
  		var location = $('#locations option:selected').text(), expires = new Date();
-        
         expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
  		document.cookie = 'location='+location+'; expires='+expires+';';
- 		console.log(location);
- 		console.log(document.cookie);
 	}
  );
 });
