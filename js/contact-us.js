e31481151a1df48eb0fba76413609ee2a5c60042
Corey Duffy <c.duffy@kainos.com>
2018-07-07 11:05:54 +0100

    Commenting JS Code

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,6 +1,8 @@
 $('#contact-form').submit(function(){
     var contactMethodSelected = $('#preferred-contact-method-checkboxes :checkbox:checked').length;
 
+    //If no checkbox has been checked then alert the user and prevent form submission. This will ensure at least one checkbox is checked.
+    //NOTE: Using alert at the minute, this will be replaced with something like Bootstrap pop-over
     if (contactMethodSelected === 0){
         alert('Please specify at least one preferred contact method');
         return false;
