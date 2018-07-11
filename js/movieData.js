6dc2434b1f65c34f5553de92c82645daf490837a
Jonathan Moore <j.moore@kainos.com>
2018-07-11 14:01:53 +0100

    Updated moviedata.js to include ratings and more movies

--- js/movieData.js
+++ js/movieData.js
@@ -2,33 +2,113 @@ var jsonData = {
   movies: [
     {
       title: "Adrift",
-      tagline: "Based on the incredible true story",
+      tagline: "'Based on the incredible true story'",
       description: "Based on the true story of survival, a young couple's chance encounter leads them first to love, and then on the adventure of a lifetime as they face one of the most catastrophic hurricanes in recorded history.",
-      cast: "Shailene Woodley, Sam Claflin, Grace Palmer ",
+      cast: "Shailene Woodley, Sam Claflin, Grace Palmer",
       director: "Baltasar Kormákur",
       genre: "Action, Adventure, Drama",
+      rating: "12A",
       poster: "Media/Movie covers/Adrift.jpg",
       locations: ["Belfast", "Antrim"]
     },
     {
+      title: "Deadpool 2",
+      tagline: "'Prepare for the second coming.'",
+      description: "After surviving a near fatal bovine attack, a disfigured cafeteria chef (Wade Wilson) struggles to fulfill his dream of becoming Mayberry's hottest bartender while also learning to cope with his lost sense of taste. Searching to regain his spice for life, as well as a flux capacitor, Wade must battle ninjas, the yakuza, and a pack of sexually aggressive canines, as he journeys around the world to discover the importance of family, friendship, and flavor - finding a new taste for adventure and earning the coveted coffee mug title of World's Best Lover.",
+      cast: "Morena Baccarin, Ryan Reynolds, Josh Brolin, Brianna Hildebrand",
+      director: "David Leitch",
+      genre: "Action, Adventure, Comedy",
+      rating: "15",
+      poster: "Media/Movie covers/Deadpool 2.jpg",
+      locations: ["Belfast", "Antrim", "Lisburn"]
+    },
+    {
+      title: "The First Purge",
+      tagline: "'Make America Great Again'",
+      description: "The film will be a prequel that will focus on the events that lead up to the very first Purge event.",
+      cast: "Marisa Tomei, Lex Scott Davis, Luna Lauren Velez",
+      director: "Gerard McMurray",
+      genre: "Action, Horror, Sci-Fi",
+      rating: "15",
+      poster: "Media/Movie covers/The first purge.jpg",
+      locations: ["Belfast", "Lisburn"]
+    },
+    {
+      title: "Hereditary",
+      tagline: "'Every family tree hides a secret'",
+      description: "When Ellen, the matriarch of the Graham family, passes away, her daughter's family begins to unravel cryptic and increasingly terrifying secrets about their ancestry.",
+      cast: "Toni Collette, Gabriel Byrne, Alex Wolff",
+      director: "Ari Aster",
+      genre: "Drama, Horror, Mystery",
+      rating: "15",
+      poster: "Media/Movie covers/Hereditary.jpg",
+      locations: ["Belfast", "Lisburn"]
+    },
+    {
+      title: "Incredibles 2",
+      tagline: "'You have powers! Yeah, baby!'",
+      description: "Bob Parr (Mr. Incredible) is left to care for Jack-Jack while Helen (Elastigirl) is out saving the world.",
+      cast: "Samuel L. Jackson, Catherine Keener, Holly Hunter, Bob Odenkirk",
+      director: "Brad Bird",
+      genre: "Animation, Action, Adventure",
+      rating: "PG",
+      poster: "Media/Movie covers/Incredibles 2.jpg",
+      locations: ["Belfast", "Antrim", "Lisburn"]
+    },
+    {
       title: "Jurassic World: Fallen Kingdom",
-      tagline: "Life finds a way",
+      tagline: "'Life finds a way'",
       description: "When the island's dormant volcano begins roaring to life, Owen and Claire mount a campaign to rescue the remaining dinosaurs from this extinction-level event.",
       cast: "Bryce Dallas Howard, Chris Pratt, Jeff Goldblum, Daniel Stisen",
       director: "J.A. Bayona",
       genre: "Action, Adventure, Sci-Fi",
+      rating: "12A",
       poster: "Media/Movie covers/Jurassic World Fallen Kingdom.jpg",
       locations: ["Belfast", "Lisburn"]
     },
     {
       title: "Ocean's 8",
-      tagline: "Every Con Has Its Pros",
+      tagline: "'Every Con Has Its Pros'",
       description: "Danny Ocean's estranged sister Debbie attempts to pull off the heist of the century at New York City's star-studded annual Met Gala. Her first stop is to assemble the perfect crew: Lou, Rose, Daphne Kluger, Nine Ball, Tammy, Amita, and Constance.",
       cast: "Olivia Munn, Cate Blanchett, Matt Damon, Dakota Fanning",
       director: "Gary Ross",
       genre: "Action, Crime, Thriller",
+      rating: "12A",
       poster: "Media/Movie covers/Oceans 8.jpg",
       locations: ["Belfast", "Antrim", "Lisburn"]
+    },
+    {
+      title: "Sicario 2: Soldado",
+      tagline: "'Day of the Soldado'",
+      description: "The drug war on the US-Mexico border has escalated as the cartels have begun trafficking terrorists across the US border. To fight the war, federal agent Matt Graver re-teams with the mercurial Alejandro.",
+      cast: "Josh Brolin, Benicio Del Toro, Isabela Moner",
+      director: "Stefano Sollima",
+      genre: "Action, Crime, Drama",
+      rating: "15",
+      poster: "Media/Movie covers/Sicario 2- Soldado.jpg",
+      locations: ["Antrim"]
+    },
+    {
+      title: "Skyscraper",
+      tagline: "'Skyscraper'",
+      description: "FBI Hostage Rescue Team leader and U.S. war veteran Will Ford, who now assesses security for skyscrapers. On assignment in Hong Kong he finds the tallest, safest building in the world suddenly ablaze, and he's been framed for it.",
+      cast: "Dwayne Johnson, Neve Campbell, Pablo Schreiber",
+      director: "Rawson Marshall Thurber",
+      genre: "Action, Crime, Drama",
+      rating: "12A",
+      poster: "Media/Movie covers/Skyscraper.jpg",
+      locations: ["Antrim", "Lisburn"]
+    },
+    {
+      title: "Tag",
+      tagline: "'Tag. You're it'",
+      description: "A small group of former classmates organize an elaborate, annual game of tag that requires some to travel all over the country.",
+      cast: "Annabelle Wallis, Jeremy Renner, Isla Fisher",
+      director: "Jeff Tomsic",
+      genre: "Comedy",
+      rating: "15",
+      poster: "Media/Movie covers/Tag.jpg",
+      locations: ["Belfast", "Antrim"]
     }
   ]
 }
