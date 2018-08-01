b3bbda40e1982864f7f99768010f09a5cfbc3a61
agildernew <aoifeg@kainos.com>
2018-08-01 22:26:29 +0100

    Adding calculations to prices on booking form

--- js/booking.js
+++ js/booking.js
@@ -1,4 +1,5 @@
 var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+var adultSubTotal = 0, studentSubTotal = 0, childSubTotal = 0;
 
 /*
 	As the page loads set the value in the #locations select equal to the value stored in the location cookie (as it has already been determined by the user).
@@ -11,6 +12,7 @@ window.onload = function() {
         setLocationCookie();
         updateBookingFields();
     });
+    generateBookingNumber();
     //Provie the user with a reference number upon submitting the form.
     //NOTE: alert used at the minute but this will be changed to use Bootstrap features when styling
     $('#booking-form').submit(function() {
@@ -41,3 +43,34 @@ function generateBookingNumber() {
     //Generate a random 5 digit booking number
     return Math.floor(Math.random()*90000) + 10000;
 }
+
+function calcuateSubTotalAdult() {
+    var x = document.getElementById("adult-quantity").value;
+    var y = 6.5;
+    adultSubTotal = x * y;
+    document.getElementById("adult-subtotal").innerHTML = "£" + adultSubTotal;
+    calculateTotal();
+}
+
+function calcuateSubTotalStudent() {
+    var x = document.getElementById("student-quantity").value;
+    var y = 5;
+    studentSubTotal = x * y;
+    document.getElementById("student-subtotal").innerHTML = "£" + studentSubTotal;
+    calculateTotal();
+}
+
+function calcuateSubTotalChild() {
+    var x = document.getElementById("child-quantity").value;
+    var y = 4;
+    childSubTotal = x * y;
+    document.getElementById("child-subtotal").innerHTML = "£" + childSubTotal;
+    calculateTotal();
+}
+
+function calculateTotal(){
+  var total = adultSubTotal + studentSubTotal + childSubTotal;
+  document.getElementById("total-price").innerHTML = "£" + total;
+}
+
+
