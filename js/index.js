73a1d66aaf9ca94229338a870e5816367822ef68
agildernew <aoifeg@kainos.com>
2018-08-03 14:30:30 +0100

    Allowing user to filter film by genre

--- js/index.js
+++ js/index.js
@@ -13,6 +13,10 @@ window.onload = function() {
         setLocationCookie();
         populateHTMLMovieData();
     });
+    $('#genre-list').on('change', function() {
+        setLocationCookie();
+        populateHTMLMovieData();    
+    });
 }
 
 function populateHTMLMovieData() {
@@ -26,26 +30,58 @@ function populateHTMLMovieData() {
     jQuery.each(jsonData.movies, function(index, movie) {
       //Only display movie if it plays at the location selected by the user
       if (jQuery.inArray(getCookie('location'), movie.locations) !== -1 || isChrome) {
-        //Format each movie object to HTML
-        data.push('<tr>' +
-          '<th class="movie-poster-container">' +
-          '<img src="' + movie.poster + '" class="movie-poster"></img>' +
-          '</th>' +
-          '<th class="movie-data">' +
-          '<h3>' + movie.title + '</h3>' +
-          '<h5>' + movie.tagline + '</h5>' +
-          '<p>Description: ' + movie.description + '</p>' +
-          '<p>Cast: ' + movie.cast + '</p>' +
-          '<p>Director: ' + movie.director + '</p>' +
-          '<p>Genre: ' + movie.genre + '</p>' +
-          '<p>Rating: ' + movie.rating + '</p>' +
-          '<p><a href="' + movie.trailer + '" target="_blank">Trailer: ' + movie.title + '</a></p>' +
-          '<p>Locations: ' + movie.locations + '</p>' +
-          '</th>' +
-          '</tr>');
+        if ($('#genre-list').val() != 'select-genre') {
+          //Genre filter set therefore display all movies by location if they are also that genre
+          var movieGenres = movie.genre;
+          var selectedGenre = $('#genre-list').val();
+          if (movieGenres.indexOf(selectedGenre) != -1 ) {
+            //Format each movie object to HTML
+            data.push('<tr>' +
+            '<th class="movie-poster-container">' +
+            '<img src="' + movie.poster + '" class="movie-poster"></img>' +
+            '</th>' +
+            '<th class="movie-data">' +
+            '<h3>' + movie.title + '</h3>' +
+            '<h5>' + movie.tagline + '</h5>' +
+            '<p>Description: ' + movie.description + '</p>' +
+            '<p>Cast: ' + movie.cast + '</p>' +
+            '<p>Director: ' + movie.director + '</p>' +
+            '<p>Genre: ' + movie.genre + '</p>' +
+            '<p>Rating: ' + movie.rating + '</p>' +
+            '<p><a href="' + movie.trailer + '" target="_blank">Trailer: ' + movie.title + '</a></p>' +
+            '<p>Locations: ' + movie.locations + '</p>' +
+            '</th>' +
+            '</tr>');
+          }
+        } else {
+          //Genre filter not set therefore display all movies by location only
+          //Format each movie object to HTML
+          data.push('<tr>' +
+            '<th class="movie-poster-container">' +
+            '<img src="' + movie.poster + '" class="movie-poster"></img>' +
+            '</th>' +
+            '<th class="movie-data">' +
+            '<h3>' + movie.title + '</h3>' +
+            '<h5>' + movie.tagline + '</h5>' +
+            '<p>Description: ' + movie.description + '</p>' +
+            '<p>Cast: ' + movie.cast + '</p>' +
+            '<p>Director: ' + movie.director + '</p>' +
+            '<p>Genre: ' + movie.genre + '</p>' +
+            '<p>Rating: ' + movie.rating + '</p>' +
+            '<p><a href="' + movie.trailer + '" target="_blank">Trailer: ' + movie.title + '</a></p>' +
+            '<p>Locations: ' + movie.locations + '</p>' +
+            '</th>' +
+            '</tr>');
+        }
       }
     });
   });
   //Push the array of HTML formatted movies to the 'movieList' Div in index.html
   $('#movie-list').append(data);
+  if (data == "") {
+    var message = 'There are no movies that match your search criteria.'
+    $('#no-films-for-criteria').text(message)
+  } else {
+    $('#no-films-for-criteria').text("")
+  }
 }
